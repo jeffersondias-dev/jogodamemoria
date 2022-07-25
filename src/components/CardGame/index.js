bf3e759cb9eb2caa25519be51af7e841a0bd6bb4
Jefferson Dias <jeff@jeffersondias.localdomain>
2022-07-25 15:17:07 -0300

    :sparkles: Now is possible change the icon

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,9 +1,9 @@
 import './style.css';
 
-function CardGame(cardLogo) {
+function CardGame(cardLogo = "field-agent", alt = "field-agent") {
     return /*html*/ `
         <article class="card-game">
-            <img src="/src/images/${cardLogo}.png" alt="${cardLogo}">
+            <img src="/src/images/${cardLogo}.png" alt="${alt}">
         </article>
     `;
 }
