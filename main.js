01485da983e81d59d52be0207890ff3121315bd0
Jefferson Dias <jeff@jeffersondias.localdomain>
2022-07-26 13:33:24 -0300

    :sparkles: Add click event on CardFrontBack component

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const $root = document.querySelector('#root');
 $root.insertAdjacentHTML(
     'beforeend',
     `   ${ScoreBoard("Player 1")}
-        ${BoardGame(2)}
+        ${BoardGame(6)}
 
 `
 );
