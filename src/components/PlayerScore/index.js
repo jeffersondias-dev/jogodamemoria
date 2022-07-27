7f0e057508f4378b1eab057d817e6f2e5787fee2
Jefferson Dias <jeff@jeffersondias.localdomain>
2022-07-27 08:21:29 -0300

    :sparkles: Create the PlayerScore component

---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,20 @@
+import './style.css';
+
+function PlayerScore() {
+    return /*html*/`
+    <div class="player-score">
+        <div class="visual-score">
+        
+        </div>
+        <div class="visual-score">
+        
+        </div>
+        <div class="visual-score">
+        
+        </div>
+    </div>
+`;
+
+}
+
+export default PlayerScore;
