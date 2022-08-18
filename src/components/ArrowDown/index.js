21d9e8f23e077d6b066af173cea07abbb82b9986
Jefferson Dias <jeff@jeffersondias.localdomain>
2022-08-18 15:57:24 -0300

    :construction: create ArrowDown component without CSS

---
+++ src/components/ArrowDown/index.js
@@ -0,0 +1,10 @@
+
+import './style.css'
+
+function ArrowDown() {
+  return /* html */ `
+   <img class="arrow-down" src="./public/images/arrow-down.png" alt="Ícone de uma seta para baixo - arrow-down" />
+  `;
+}
+
+export default ArrowDown;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -2,10 +2,12 @@ import './style.css';
 import PlayerName from '../../components/PlayerName';
 import PlayerScore from '../../components/PlayerScore';
 import VsPlayer from '../../components/VsPlayer';
+import ArrowDown from '../../components/ArrowDown';
 
 function ScoreBoard(content) {
     return /*html*/ `
     <header class="score-board">
+        ${ArrowDown()}
         ${PlayerName("Player 1")}
         ${PlayerScore()}
         ${VsPlayer()}
