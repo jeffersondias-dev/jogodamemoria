f1ae33b1a603e959d0430b4f3e880abc0b1f3ed0
Jefferson Dias <96580079+jeffersondias-dev@users.noreply.github.com>
2022-09-12 12:37:00 -0300

    update

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -7,7 +7,6 @@ import ArrowDown from '../../components/ArrowDown';
 function ScoreBoard(content) {
     return /*html*/ `
     <header class="score-board">
-        ${ArrowDown()}
         ${PlayerName("Player 1")}
         ${PlayerScore()}
         ${VsPlayer()}
