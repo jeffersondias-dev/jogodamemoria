887dcd405771ecbd9402f298e836515853073c00
Jefferson Dias - Dev <jeffersonlaradias@outlook.com>
2022-09-15 18:06:25 -0300

    :construction: add random cards

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -3,8 +3,8 @@ import CardFrontBack from '../../components/CardFrontBack';
 import cards from "./data";
 
 function BoardGame(amountCards) {
-    
-    
+
+
     const flipAndHideCards = ($cardsActive) => {
         $cardsActive.forEach((card) => card.classList.remove('-active'));
     };
@@ -12,16 +12,17 @@ function BoardGame(amountCards) {
     const swapPlayer = () => {
         const $arrowDown = document.querySelector('.arrow-down');
         const currentPlayer = $arrowDown.getAttribute('data-currentPlayer');
-        
+
         $arrowDown.setAttribute('data-currentPlayer', currentPlayer == 1 ? 2 : 1);
     };
-    
+
+
     window.boardGame = {}
     window.boardGame.handleClick = () => {
 
         const $boardGame = document.querySelector('.board-game');
         const $cardsActive = $boardGame.querySelectorAll('.card-front-back.-active')
-        
+
         if ($cardsActive.length === 2) {
             setTimeout(() => {
                 flipAndHideCards($cardsActive);
@@ -32,8 +33,19 @@ function BoardGame(amountCards) {
             flipAndHideCards($cardsActive);
         }
     }
-    
+
+
+    const randomCards = (htmlCardsList) => {
+        for (let i = 0; i < htmlCardsList.length; i++) {
+            let randomNumber = Math.floor(Math.random() * htmlCardsList.length);
+            let aux = htmlCardsList[randomNumber];
+            htmlCardsList[randomNumber] = htmlCardsList[i];
+            htmlCardsList[i] = aux;
+        }
+    };
+
     const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon));
+    randomCards(htmlCardsList);
     const $htmlCards = htmlCardsList.join('');
 
     return /*html*/`
