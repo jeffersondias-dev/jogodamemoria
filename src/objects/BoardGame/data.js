239b70629840eff10d715474b7d0d7c22f2b8eb1
Jefferson Dias - Dev <jeffersonlaradias@outlook.com>
2022-09-15 07:23:56 -0300

    :construction: Add new icons

--- src/objects/BoardGame/data.js
+++ src/objects/BoardGame/data.js
@@ -1,52 +1,68 @@
 const cards = [
     {
         icon: "crown", 
-        altIcon: "a crown icon"
+        altIcon: "a crown icon, Um icone de uma coroa"
     },
     {
         icon: "diamond", 
-        altIcon: "a start icon"
+        altIcon: "a start icon, Um icone de um diamante"
     },
     {
         icon: "letter", 
-        altIcon: "a letter icon"
+        altIcon: "a letter icon, Um icone de uma carta"
     },
     {
         icon: "lightning", 
-        altIcon: "a lightning icon"
+        altIcon: "a lightning icon, Um icone de um raio"
     },
     {
         icon: "star", 
-        altIcon: "a star icon"
+        altIcon: "a star icon, Um icone de uma estrela"
     },
     {
         icon: "trophy", 
-        altIcon: "a trophy icon"
+        altIcon: "a trophy icon, Um icone de um troféu"
+    },
+    {
+        icon: "redPotion", 
+        altIcon: "a Red Potion icon, Um icone de uma garrafa na cor azul"
+    },
+    {
+        icon: "bluePotion", 
+        altIcon: "a Blue potion icon | Um icone de uma garrafa na cor vermelha"
     },
     // second icons
     {
         icon: "crown", 
-        altIcon: "a crown icon"
+        altIcon: "a crown icon, Um icone de uma coroa"
     },
     {
         icon: "diamond", 
-        altIcon: "a start icon"
+        altIcon: "a start icon, Um icone de um diamante"
     },
     {
         icon: "letter", 
-        altIcon: "a letter icon"
+        altIcon: "a letter icon, Um icone de uma carta"
     },
     {
         icon: "lightning", 
-        altIcon: "a lightning icon"
+        altIcon: "a lightning icon, Um icone de um raio"
     },
     {
         icon: "star", 
-        altIcon: "a star icon"
+        altIcon: "a star icon, Um icone de uma estrela"
     },
     {
         icon: "trophy", 
-        altIcon: "a trophy icon"
+        altIcon: "a trophy icon, Um icone de um troféu"
+    },
+    {
+        icon: "redPotion", 
+        altIcon: "a Red potion icon, Um icone de uma garrafa na cor azul"
+    },
+    {
+        icon: "bluePotion", 
+        altIcon: "a Blue potion icon | Um icone de uma garrafa na cor vermelha"
     }
 ];
 
